4bf6b30995a50d4a8e67533e0637d60a03987f8c
Pedro Costa <pedrosalvacaocosta@gmail.com>
2018-01-25 14:52:50 +0000

    Added more test listeners

---
+++ kafka-proto-api/src/test/resources/processor/MessagingListener_MessageType_Incompatible.java
@@ -0,0 +1,18 @@
+package processor;
+
+import pcosta.kafka.api.MessageListener;
+import pcosta.kafka.api.MessageMetadata;
+import pcosta.kafka.spring.annotation.MessagingListener;
+import pcosta.kafka.spring.annotation.MessagingListenerProcessorTest;
+
+/**
+ * @author Pedro Costa
+ */
+@MessagingListener(topic = "Topic", message = MessagingListenerProcessorTest.TestMessage.class)
+public class MessagingListener_MessageType_Incompatible implements MessageListener<String> {
+
+    @Override
+    public void onMessage(final MessageMetadata messageMetadata, final String message) {
+        // empty
+    }
+}
